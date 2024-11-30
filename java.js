c2467ff476cf3689e230389a2d00fca488c477fe
eduardandrde <andrade.maria@escolar.ifrn.edu.br>
2024-11-30 15:35:38 -0300

    .

--- java.js
+++ java.js
@@ -1,31 +1,12 @@
-const perguntasF = document.querySelectorAll(".perguntas-frequentes");
+const abrirModal = document.getElementById("abrirModal");
+const modalContainer = document.getElementById("modalContainer");
+const buttonFechar = document.getElementById("buttonFechar");
 
-perguntasF.forEach((perguntaFrequente) => {
-    const pergunta = perguntaFrequente.querySelector(".pergunta");
-    const icone = pergunta.querySelector("i");
-    const resposta = perguntaFrequente.querySelector(".resposta");
-
-    pergunta.addEventListener("click", () => {
-        // Fechar todas as outras respostas
-        perguntasF.forEach((outroPerguntaFrequente) => {
-            const outraResposta = outroPerguntaFrequente.querySelector(".resposta");
-            const outroIcone = outroPerguntaFrequente.querySelector("i");
-
-            // Fechar as respostas e mostrar ícones de "+" em outras perguntas
-            if (outroPerguntaFrequente !== perguntaFrequente) {
-                outraResposta.classList.remove("mostrar");
-                outroIcone.classList.add("fa-circle-plus");
-                outroIcone.classList.remove("fa-circle-minus");
-            }
-        });
-
-        // Alternar a visibilidade da resposta clicada
-        resposta.classList.toggle("mostrar");
-
-        // Alternar o ícone entre "+" e "-"
-        icone.classList.toggle("fa-circle-plus");
-        icone.classList.toggle("fa-circle-minus");
-    });
+abrirModal.addEventListener("click",()=>{
+    modalContainer.classList.add("aparecer");
+});
+buttonFechar.addEventListener("click",()=>{
+    modalContainer.classList.remove("aparecer");
 });
 
 
