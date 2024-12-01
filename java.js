3ddc0eef67de6420966a76440774eaac58d0bf0b
eduardandrde <andrade.maria@escolar.ifrn.edu.br>
2024-12-01 19:02:01 -0300

    .

--- java.js
+++ java.js
@@ -1,13 +1,50 @@
-const abrirModal = document.getElementById("abrirModal");
-const modalContainer = document.getElementById("modalContainer");
-const buttonFechar = document.getElementById("buttonFechar");
+// Abrir e fechar o menu de acessibilidade
+document.querySelector('.botao-acessibilidade').addEventListener('click', function () {
+    const panel = document.querySelector('.painel-acessibilidade');
+    panel.style.display = panel.style.display === 'block' ? 'none' : 'block';
+});
 
-abrirModal.addEventListener("click",()=>{
-    modalContainer.classList.add("aparecer");
-});
-buttonFechar.addEventListener("click",()=>{
-    modalContainer.classList.remove("aparecer");
-});
+function DislexiaFont() {
+    const body = document.body;
+    if (body.classList.contains('dislexia-font')) {
+      body.classList.remove('dislexia-font'); 
+    } else {
+      body.classList.add('dislexia-font'); 
+    }
+}
+function destacarTitulos() {
+    // Seleciona todos os títulos 
+    const todosTitulos = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+    // Verifica se os títulos já estão destacados
+    const jaDestacados = todosTitulos[0]?.classList.contains('titulo-destaque');
+    // Adiciona ou remove o destaque
+    if (jaDestacados) {
+        todosTitulos.forEach(titulo => {
+            titulo.classList.remove('titulo-destaque');
+        });
+    } else {
+        todosTitulos.forEach(titulo => {
+            titulo.classList.add('titulo-destaque');
+        });
+    }
+}
+function destacarLinks() {
+    // Seleciona todos os links na página
+    const todosLinks = document.querySelectorAll('a');
+    // Verifica se algum link já tem a classe destaque-links
+    const umLink = todosLinks[0].classList.contains('destaque-links');
+    // Se os links já estão destacados remove o destaque
+    if (umLink) {
+        todosLinks.forEach(link => {
+            link.classList.remove('destaque-links');
+        });
+    } else {
+        // Se os links não estão destacados vai adicionar o destaque
+        todosLinks.forEach(link => {
+            link.classList.add('destaque-links');
+        });
+    }
+}
 
 
 
